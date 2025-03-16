204eeb76605bc7044205a90e57b8aedbf245a4b2
ruben61187 <66740429+ruben61187@users.noreply.github.com>
2025-03-16 16:36:22 +0100

    Segunda sesion curso

--- mock-srv/plugins/data-utils.js
+++ mock-srv/plugins/data-utils.js
@@ -1,9 +1,7 @@
 "use strict";
 import fp from "fastify-plugin";
-import { promisify } from "node:util";
+import { PassThrough } from "node:stream";
 
-// Promisify setTimeout
-const timeout = promisify(setTimeout);
 
 // Mock data
 const orders = {
@@ -18,18 +16,15 @@ const catToPrefix = {
   confectionery: "B",
 };
 
-// Simulate realtime orders
-async function* realtimeOrdersSimulator() {
-  const ids = Object.keys(orders);
-  while (true) {
-    const delta = Math.floor(Math.random() * 7) + 1;
-    const id = ids[Math.floor(Math.random() * ids.length)];
-    orders[id].total += delta;
-    const { total } = orders[id];
-    yield JSON.stringify({ id, total });
-    await timeout(1500);
- }
-}
+ // Create a stream of orders
+ const orderStream = new PassThrough({ objectMode: true });
+
+ // Simulate real-time orders
+ async function* realtimeOrdersSimulator() {
+   for await (const { id, total } of orderStream) {
+     yield JSON.stringify({ id, total });
+   }
+ } 
 
 // Return current orders
 function* currentOrders(category) {
@@ -48,10 +43,29 @@ const calculateID = (idPrefix, data) => {
   return `${idPrefix}${next}`;
 };
 
+// Add order to stream and update total
+function addOrder(id, amount) {
+  if (orders.hasOwnProperty(id) === false) {
+    const err = new Error(`Order ${id} not found`);
+    err.status = 404;
+    throw err;
+  }
+  if (Number.isInteger(amount) === false) {
+    const err = new Error('Supplied amount must be an integer');
+    err.status = 400;
+    throw err;
+  }
+  orders[id].total += amount;
+  const { total } = orders[id]
+  console.log("Adding order: %o", { id, total });
+  orderStream.write({ id, total });
+} 
+
 // Plugin
 export default fp(async function (fastify, opts) {
   fastify.decorate("currentOrders", currentOrders);
   fastify.decorate("realtimeOrders", realtimeOrdersSimulator);
+  fastify.decorate("addOrder", addOrder);
   fastify.decorate("mockDataInsert", function (request, category, data) {
     const idPrefix = catToPrefix[category];
     const id = calculateID(idPrefix, data);
--- static/app.js
+++ static/app.js
@@ -37,18 +37,24 @@ const category = document.querySelector("#category");
 const add = document.querySelector("#add");
 
 let socket = null;
-// Realtime orders handler using WebSocket
+// Realtime orders via Websocket
 const realtimeOrders = (category) => {
-  if (socket) socket.close();
-  socket = new WebSocket(`${WS_API}/orders/${category}`);
+  if (socket === null) {
+    socket = new WebSocket(`${WS_API}/orders/${category}`);
+  } else {
+    socket.send(
+      // Send update-category command to server
+      JSON.stringify({ cmd: "update-category", payload: { category } })
+    );
+  }
+  // Listen for messages
   socket.addEventListener("message", ({ data }) => {
     try {
       const { id, total } = JSON.parse(data);
       const item = document.querySelector(`[data-id="${id}"]`);
       if (item === null) return;
       const span =
-        item.querySelector('[slot="orders"]')
-         || document.createElement("span");
+        item.querySelector('[slot="orders"]') || document.createElement("span");
       span.slot = "orders";
       span.textContent = total;
       item.appendChild(span);
